7bfdcb9abe31585701472de60e1ab99f08e69846
afaq <afaqak124@gmail.com>
2023-09-03 06:01:00 +0500

    feat:onboarding expert

--- src/app/on-boarding/expert/page.js
+++ src/app/on-boarding/expert/page.js
@@ -2,37 +2,76 @@
 import React, { useState } from 'react';
 import { Modal, AvailableDays } from '@/components';
 import Image from 'next/image';
-
+import { onBoardTutor } from '@/features/onBoard/onBoardThunk';
+import { useDispatch } from 'react-redux';
+import { useRouter } from 'next/navigation';
+import { showSuccessToast, showErrorToast } from '@/utils/toastUtility';
 const OnBoardingExpert = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [expertise, setExpertise] = useState([]);
   const [modelOpen, setModalOpen] = useState(false);
+  const [hourlyRate, setHourlyRate] = useState(0);
   const handleModalSubmit = (val) => {
     setExpertise([...expertise, val]);
     setModalOpen(false);
   };
+  const [availability, setAvailability] = useState([]);
+
+  const onSuccess = () => {
+    showSuccessToast(router, setLoading, 'You are now a tutor! 🎉', '/');
+  };
+
+  const onError = (error) => {
+    showErrorToast(setLoading, 'Oops! Something went wrong. 😞');
+
+    console.error('Error creating user:', error);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!hourlyRate) return;
+    try {
+      setLoading(true);
+      const formData = {
+        hourly_rate: hourlyRate,
+        expertise: [1, 6, 2],
+        availability,
+      };
+
+      dispatch(onBoardTutor({ formData, onSuccess, onError }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="h-[90vh] items-center justify-center flex">
-      <div className="p-7 min-h-[70vh] rounded-lg shadow-lg bg-white w-[70%] flex flex-col gap-2">
+      <form
+        onSubmit={handleSubmit}
+        className="p-7 min-h-[70vh] rounded-lg shadow-lg bg-white w-[70%] flex flex-col gap-2"
+      >
         <h1 className="font-semibold text-lg">Complete Your Profile!</h1>
         <div>
           <div className="flex gap-2 ">
             <div className="flex flex-col w-fit">
               <label className="font-thin mb-1 uppercase text-sm">HOURLY RATE</label>
               <input
+                onChange={(e) => setHourlyRate(e.target.value)}
                 placeholder="Enter Amount"
                 type="text"
-                className="shadow-[inset_2px_2px_8px_rgba(0,0,0,0.1)] rounded-md px-2 py-1 placeholder:text-sm border-none focus:outline-none"
-                name="email"
+                className="shadow-[inset_2px_1px_6px_rgba(0,0,0,0.2)] rounded-md px-2 py-1 placeholder:text-sm border-none focus:outline-none"
+                name="hourlyRate"
               />
             </div>
             <div className="flex flex-col">
               <div className="flex gap-1">
-                <label className="text-[#616161] font-thin">EXPERTISE</label>
+                <label className="text-[#616161] font-thin text-sm">EXPERTISE</label>
                 <Image
                   onClick={() => setModalOpen(true)}
-                  src={'/svgs/add_box.svg'}
-                  className="cursor-pointer"
+                  src={'/svgs/add_time_purple.svg'}
+                  className={`cursor-pointer`}
                   alt="add Interest"
                   width={20}
                   height={20}
@@ -59,10 +98,16 @@ const OnBoardingExpert = () => {
           </div>
           <div className="mt-2 ">
             <h2 className="text-[#616161] font-thin">Availability</h2>
-            <AvailableDays />
+            <AvailableDays setAvailability={setAvailability} />
           </div>
         </div>
-      </div>
+        <div className="flex justify-end">
+          <button type="submit" className="py-1 border-2 px-16 rounded-md shadow-md border-black">
+            Complete
+          </button>
+        </div>
+      </form>
+
       <Modal
         isOpen={modelOpen}
         onClose={() => setModalOpen(false)}
--- src/components/common/Availability.js
+++ src/components/common/Availability.js
@@ -4,7 +4,8 @@ import TimePicker from 'rc-time-picker';
 import 'rc-time-picker/assets/index.css';
 import moment from 'moment';
 import Image from 'next/image';
-export const AvailableDays = () => {
+
+export const AvailableDays = ({ setAvailability }) => {
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   const initialSchedule = {};
@@ -14,17 +15,25 @@ export const AvailableDays = () => {
   });
 
   const [schedule, setSchedule] = useState(initialSchedule);
-  console.log(schedule);
+  const [checkboxes, setCheckboxes] = useState({});
 
   const handleAddTimeSlot = (day) => {
-    const newSlot = {
-      day,
-      from: 0,
-      until: 0,
-    };
-    setSchedule((prevSchedule) => ({
-      ...prevSchedule,
-      [day]: [...prevSchedule[day], newSlot],
+    if (checkboxes[day]) {
+      const newSlot = {
+        from: 0,
+        until: 0,
+      };
+      setSchedule((prevSchedule) => ({
+        ...prevSchedule,
+        [day]: [...prevSchedule[day], newSlot],
+      }));
+    }
+  };
+
+  const handleCheckboxChange = (day, checked) => {
+    setCheckboxes((prevCheckboxes) => ({
+      ...prevCheckboxes,
+      [day]: checked,
     }));
   };
 
@@ -32,53 +41,82 @@ export const AvailableDays = () => {
     setSchedule((prev) => ({
       ...prev,
       [day]: prev[day].map((slot, index) =>
-        index === indexSlot ? { ...slot, [field]: time } : slot
+        index === indexSlot ? { ...slot, [field]: time, day } : slot
       ),
     }));
+    formatScheduleData();
+  };
+
+  const formatScheduleData = () => {
+    const availability = [];
+    daysOfWeek.forEach((day) => {
+      schedule[day].forEach((slot) => {
+        if (slot.from && slot.until) {
+          availability.push({
+            day: day.toLowerCase(),
+            from: slot.from.format('HH:mm'),
+            to: slot.until.format('HH:mm'),
+          });
+        }
+      });
+    });
+
+    setAvailability(availability);
   };
 
   return (
-    <div className="mt-4 min-h-fit">
-      <div className="flex flex-col">
+    <div className="mt-1 min-h-fit">
+      <div className="flex flex-col text-[#616161]">
         {daysOfWeek.map((day) => (
-          <div key={day} className="flex mr-4">
-            <div class="flex items-center mr-2 mb-4">
+          <div key={day} className="flex items-center mr-4">
+            <div className="flex items-center mr-2">
               <input
-                id="default-checkbox"
+                id={`checkbox-${day}`}
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded "
+                className="w-4 h-4 text-blue-600 mb-1 bg-gray-100 border-gray-300 rounded"
+                onChange={(e) => handleCheckboxChange(day, e.target.checked)}
               />
             </div>
             <div className="mb-2 mr-3 text-sm w-[4%]">{day}</div>
             {schedule[day].map((slot, slotIndex) => (
               <div key={slotIndex} className="flex text-sm mb-2 gap-3 mr-2">
                 <div className="w-16">
-                  {' '}
                   <TimePicker
-                    className="w-full custom-time-picker"
+                    className="w-full custom-time-picker text-sm"
                     format="HH:mm"
                     placeholder="From"
+                    showSecond={false}
+                    clearIcon={false}
                     onChange={(time) => handleTimeChange(time, day, 'from', slotIndex)}
                     value={slot.from ? moment(slot.from, 'HH:mm') : null}
+                    disabled={!checkboxes[day]} // Disable TimePicker if checkbox is not checked
                   />
                 </div>
-                <div className="w-16 text-sm">
+                <div className="w-16">
                   <TimePicker
-                    className="w-full custom-time-picker"
+                    className="w-full custom-time-picker text-sm flex"
+                    showSecond={false}
+                    clearIcon={false}
                     format="HH:mm"
                     placeholder="Until"
                     onChange={(time) => handleTimeChange(time, day, 'until', slotIndex)}
                     value={slot.until ? moment(slot.until, 'HH:mm') : null}
+                    disabled={!checkboxes[day]} // Disable TimePicker if checkbox is not checked
                   />
                 </div>
               </div>
             ))}
             <div>
-              <button className="py-1 rounded-md" onClick={() => handleAddTimeSlot(day)}>
+              <button
+                type="button"
+                className="py-1 rounded-md"
+                onClick={() => handleAddTimeSlot(day)}
+                disabled={!checkboxes[day]}
+              >
                 <Image
-                  src={'/svgs/add_box.svg'}
-                  className="cursor-pointer"
+                  src={'/svgs/add_time_purple.svg'}
+                  className={`cursor-pointer -mt-1 ${!checkboxes[day] ? 'opacity-50' : ''}`}
                   alt="add Interest"
                   width={20}
                   height={20}
