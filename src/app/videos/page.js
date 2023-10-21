6c6d0efefe80700314ab331319e6bbec6aa43d85
afaq <afaqak124@gmail.com>
2023-10-21 23:23:44 +0500

    saving

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -2,7 +2,7 @@ import axios from '../../utils/index';
 import { VideoGallery } from '@/components/video/videoGallery';
 
 const Videos = async () => {
-  const getVideos = await axios.get('/assets/videos');
+  const getVideos = await axios.get('http://localhost:4000/assets/videos');
   console.log(getVideos.data, 'getVideos');
   if (!getVideos.data) {
     <div>no videos!</div>;
