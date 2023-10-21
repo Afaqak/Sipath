cf878259097e463b3b9e9a728b4ff91a7a81fa10
afaq <afaqak124@gmail.com>
2023-10-21 23:17:45 +0500

    saving

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -4,6 +4,9 @@ import { VideoGallery } from '@/components/video/videoGallery';
 const Videos = async () => {
   const getVideos = await axios.get('/assets/videos');
   console.log(getVideos.data, 'getVideos');
+  if (!getVideos.data) {
+    <div>no videos!</div>;
+  }
 
   return <VideoGallery videos={getVideos.data} />;
 };
