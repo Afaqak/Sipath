34f78e520de3b8e426131102720f80f1d42bb74e
afaq <afaqak124@gmail.com>
2023-10-21 23:52:38 +0500

    saving

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 import { VideoGallery } from '@/components/video/videoGallery';
 
 const Videos = async () => {
-  const getVideos = await axios.get('http://localhost:4000/assets/videos');
-  console.log(getVideos.data, 'getVideos');
-  if (!getVideos.data) {
-    <div>no videos!</div>;
-  }
+  // const getVideos = await axios.get('http://localhost:4000/assets/videos');
+  // console.log(getVideos.data, 'getVideos');
+  // if (!getVideos.data) {
+  //   <div>no videos!</div>;
+  // }
 
-  return <VideoGallery videos={getVideos.data} />;
+  // return <VideoGallery videos={getVideos.data} />;
+  return <VideoGallery />;
 };
 
 export default Videos;
--- src/components/video/videoGallery.js
+++ src/components/video/videoGallery.js
@@ -1,8 +1,24 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Video, LoadingSkeletons } from '@/components';
-
-export const VideoGallery = ({ videos }) => {
+import axios from '../../utils/index';
+export const VideoGallery = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchVideos = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get('/assets/videos');
+        setVideos(response.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchVideos();
+  }, []);
   return (
     <div className="pt-8 pb-8 overflow-visible relative w-[90%] mx-auto">
       {loading && <LoadingSkeletons times={10} />}
