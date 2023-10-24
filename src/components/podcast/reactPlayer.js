33b530c1bff660b8073fd57c4f0fd4c40a479957
afaq <afaqak124@gmail.com>
2023-10-25 00:32:58 +0500

    fixed issues with video player and comments

--- src/components/podcast/reactPlayer.js
+++ src/components/podcast/reactPlayer.js
@@ -14,7 +14,7 @@ class NextButton extends videojs.getComponent('Button') {
     this.el().innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right-to-line"><path d="M17 12H3"/><path d="m11 18 6-6-6-6"/><path d="M21 5v14"/></svg>
   `;
-    this.el().style.marginLeft = '20px'; // Adjust the margin as needed
+    this.el().style.marginLeft = '20px';
   }
 
   handleClick(event) {
